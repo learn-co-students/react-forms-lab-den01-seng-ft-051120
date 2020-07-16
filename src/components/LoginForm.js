112236ff7b6393f4c4737cadbbe506cbbd3dd447
jdbuysse <jdbuysse@gmail.com>
2020-07-16 10:58:32 -0600

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,40 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
+
+  saveState = (event) =>{
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+    
+  }
+
+  validateSubmit = (event) =>{
+    event.preventDefault()
+    if (!(this.state.username || this.state.password) === '') { 
+      this.props.handleLogin(this.state)
+    }
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.validateSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.saveState} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.saveState}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      charsLeft: 280,
+      message: ''
+    };
+  }
+
+  handleChange = event =>{
+    console.log(event)
+    this.setState({
+      [event.target.name]: event.target.value,
+      charsLeft: 280-event.target.value.length
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event => this.handleChange(event)} />
+        <p>You have {this.state.charsLeft} chars left</p>
       </div>
     );
   }
